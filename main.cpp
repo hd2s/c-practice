858136cba3b08774583c2ebc80012eb655c7233f
hd2s <harshd2singh@gmail.com>
2024-03-21 22:22:17 +0530

    switch cases

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int day;
+    cin>>day;
+    switch (day) {
+        case 1:
+            cout<<"monday";
+            break;
+        case 2:
+            cout<<"tuesday";
+            break;
+    }
+}
